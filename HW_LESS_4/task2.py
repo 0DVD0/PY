3c46d4d5edba973bbfa55817e4e27bba11f8995a
0DVD0 <161175754+0DVD0@users.noreply.github.com>
2024-04-09 01:45:00 +0300

    Added the HW 4_task2

---
+++ HW_LESS_4/task2.py
@@ -0,0 +1,139 @@
+# Aceasta este a doua ta sarcină legată a leciei legate de stringuri în python
+
+# Creează o variabilă numită `name` și seteaz-o la numele tău
+
+# CODUL TĂU VINE MAI JOS:
+name = 'David'
+# CODUL TĂU VINE MAI SUS:
+
+# Acum afișează valoarea variabilei `name` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+print(name, '\n')
+# CODUL TĂU VINE MAI SUS:
+
+# Acum creează o nouă variabilă numită `name2` și seteaz-o la valoarea variabilei `name` și afișează valoarea
+# variabilei `name2` folosind funcția `print`
+
+# CODUL TĂU VINE MAI JOS:
+name2 = name
+print(name2, '\n')
+# CODUL TĂU VINE MAI SUS:
+
+# Acum printează ultimul caracter al variabilei `name` folosind indexarea
+
+# CODUL TĂU VINE MAI JOS:
+print(name[len(name) - 1], '\n')
+# CODUL TĂU VINE MAI SUS:
+
+# Acum printează primele 3 caractere ale variabilei `name` folosind slicing
+
+# CODUL TĂU VINE MAI JOS:
+print(name[:3], '\n')
+# CODUL TĂU VINE MAI SUS:
+
+# Acum printează valoarea variabilei `name` în majuscule folosind metoda `upper`
+
+# CODUL TĂU VINE MAI JOS:
+print(name.upper(), '\n')
+# CODUL TĂU VINE MAI SUS:
+
+# Acum printează valoarea variabilei `name` în minuscule folosind metoda `lower`
+
+# CODUL TĂU VINE MAI JOS:
+print(name.lower(), '\n')
+# CODUL TĂU VINE MAI SUS:
+
+# Acum printează concatenarea variabilelor `name` și `name2`
+
+# CODUL TĂU VINE MAI JOS:
+print(name + name2, '\n')
+# CODUL TĂU VINE MAI SUS:
+
+# Creează o variabilă `text` și setează-i un text la alegere, cu restricția ca acesta să conțină mai multe rânduri
+
+# CODUL TĂU VINE MAI JOS:
+text = 'Big chungus\nSmall chungus\nAvarage sized chungus\n'
+# CODUL TĂU VINE MAI SUS:
+
+# Verifică dacă variabila `text` conține cuvântul `python`
+
+# CODUL TĂU VINE MAI JOS:
+if text.find('python') == -1:
+    print("The word python hasn't been found\n")
+else:
+    print("The word python has been found\n")
+# CODUL TĂU VINE MAI SUS:
+
+# Folosește metoda replace pentru a înlocui litera `a` din variabila `text` cu litera `e`
+
+# CODUL TĂU VINE MAI JOS:
+print(text.replace('a', 'e'), '\n')
+# CODUL TĂU VINE MAI SUS:
+
+# Folosește metoda split pentru a transforma variabila `text` într-o listă de cuvinte
+
+# CODUL TĂU VINE MAI JOS:
+print(text.split(), '\n')
+# CODUL TĂU VINE MAI SUS:
+
+# Creează un f-string care să conțină variabilele `name` și `name2`
+
+# CODUL TĂU VINE MAI JOS:
+f_string = f"Hello,Ma numesc {name}, numele meu este {name2}!"
+# CODUL TĂU VINE MAI SUS:
+
+# Verifică dacă string-ul creat se termină cu `!`
+
+# CODUL TĂU VINE MAI JOS:
+if f_string[-1] == '!':
+    print('Este !\n')
+else:
+    print('Nu-i !\n')
+# CODUL TĂU VINE MAI SUS:
+
+# Verifică dacă string-ul creat începe cu `Hello`
+
+# CODUL TĂU VINE MAI JOS:
+if f_string.find('Hello') == -1:
+    print("Nu se incepe cu 'Hello'\n")
+else:
+    print("Se incepe cu 'Hello'\n")
+# CODUL TĂU VINE MAI SUS:
+
+# Identifică indexul unde se află `!` în string-ul creat
+
+# CODUL TĂU VINE MAI JOS:
+print(f_string.index('!'), '\n')
+# CODUL TĂU VINE MAI SUS:
+
+# Identifică indexul unde se află `o` în string-ul creat folosind altă metodă
+
+# CODUL TĂU VINE MAI JOS:
+for index in range(len(f_string)):
+    if f_string[index] == 'o':
+        print(index, '\n')
+        break
+
+# CODUL TĂU VINE MAI SUS:
+
+# Utilizând format string-uri, creează un string care să conțină variabilele `name` și `name2`
+
+# CODUL TĂU VINE MAI JOS:
+form_string = "Yes my name is {}, you can call me {}\n"
+print(form_string.format(name, name2))
+# CODUL TĂU VINE MAI SUS:
+
+# Concatenează string-ul creat cu string-ul `text`
+
+# CODUL TĂU VINE MAI JOS:
+comb_word = text + form_string
+# CODUL TĂU VINE MAI SUS:
+
+# Afișează lungimea string-ului creat
+
+# CODUL TĂU VINE MAI JOS:
+print(len(comb_word))
+# CODUL TĂU VINE MAI SUS:
+
+# Aceasta a fost tot pentru această sarcină
